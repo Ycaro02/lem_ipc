48df7d3a9c318c8a12f5325c39a02c4237c793c3
Ycaro02 <ycaro02@hotmailfr>
2024-03-11 10:47:59 +0100

    test shared memory segment

--- rsc/sh/check_ipcs_free.sh
+++ rsc/sh/check_ipcs_free.sh
@@ -3,11 +3,11 @@
 source $PWD/rsc/sh/color.sh
 
 check_ipcs_free() {
-	local ipcs_free=$(ipcs -m | grep -v "0x" | wc -l)
-	if [ $ipcs_free -ne 4 ]; then
-		display_color_msg ${RED} "Ipc leak found."
+	local ipcs_free=$(ipcs -m | grep -e "0x" | wc -l)
+	if [ $ipcs_free -ne 0 ]; then
+		display_color_msg ${RED} "Shared memory segment leak found."
 	else
-		display_color_msg ${GREEN} "No IPC leak."
+		display_color_msg ${GREEN} "No Shared memory segment leak."
 	fi
 }
 
--- src/start_ipc.c
+++ src/start_ipc.c
@@ -1,5 +1,20 @@
 # include "../include/lem_ipc.h"
 
+/**
+ *	@brief Print an error message and the error string of a syscall
+ *	@param syscall_name The name of the syscall
+*/
+void syscall_perror(char *syscall_name)
+{
+	ft_printf_fd(2, "%s failed\n", syscall_name);
+	perror(syscall_name);
+}
+
+/**
+ *	@brief Initialize a shared memory segment
+ *	@param path Path to the file to use ftok
+ *	@return The shared memory id
+*/
 int init_shared_memory(char *path)
 {
 	key_t key;
@@ -8,24 +23,29 @@ int init_shared_memory(char *path)
 	errno = 0;
 	key = ftok(path, 42);
 	if (key == -1) {
-		ft_printf_fd(2, "ftok failed\n");
-		perror("ftok");
+		syscall_perror("ftok");
 		return (-1);
 	}
-	shmid = shmget(key, SHARED_MEMORY_SIZE, IPC_CREAT | 0666);
+	errno = 0;
+	/* IPC_EXCL: Fail if key exists protect against double creation ? */
+	shmid = shmget(key, SHARED_MEMORY_SIZE, (IPC_CREAT | IPC_EXCL | 0666));
 	if (shmid == -1) {
-		ft_printf_fd(2, "shmget failed\n");
+		syscall_perror("shmget");
 		return (-1);
 	}
 	return (shmid);
 }
 
+/**
+ *	@brief Destroy a shared memory segment
+ *	@param shmid The shared memory id
+ *	@return 0 on success, -1 on error
+*/
 int destroy_shared_memory(int shmid)
 {
 	errno = 0;
 	if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-		ft_printf_fd(2, "shmctl failed\n");
-		perror("shmctl");
+		syscall_perror("shmget");
 		return (-1);
 	}
 	return (0);
