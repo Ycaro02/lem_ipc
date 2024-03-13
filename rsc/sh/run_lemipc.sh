c6f5e1546e97f611b46747ecccdf5ed1569fb320
Ycaro02 <ycaro02@hotmailfr>
2024-03-13 11:41:44 +0100

    stress test 100 program with display still ok

--- rsc/sh/run_lemipc.sh
+++ rsc/sh/run_lemipc.sh
@@ -12,52 +12,46 @@ send_sigint() {
 	kill -2 ${PID}
 }
 
-sigint_test() {
-	display_color_msg ${LIGHT_BLUE} "Running lemipc basic test ..."
+send_sigint_all() {
+	display_color_msg ${YELLOW} "Sending SIGINT to all pid ..."
+	for pid in $(cat pid.txt)
+	do
+		# display_color_msg ${YELLOW} "Sending SIGINT to pid: ${pid}"
+		send_sigint ${pid}
+		sleep 0.1
+	done
 	
-	${LEMIPC} 1 &
-	pid1=$!
-	sleep 1
+	if [ -f "pid.txt" ]; then
+		rm pid.txt
+	fi
+}
 
-	${LEMIPC} 8 &
-	pid2=$!
-	sleep 1
+sigint_loop_test() {
+	for i in {1..99}
+	do
+		${LEMIPC} ${i} &
+		local loc_pid=$!
+		echo ${loc_pid} >> pid.txt
+		sleep 0.1
+	done
 
-	${LEMIPC} 19 &
-	pid3=$!
-	sleep 1
-
-	${LEMIPC} 88 &
-	pid4=$!
-	sleep 1
-
-	${LEMIPC} 42 &
-	pid5=$!
 	
 	display_color_msg ${YELLOW} "Lauch display handler ..."
 	./lemipc_display &
-	pid6=$!
-	
+	local display_pid=$!
 	display_color_msg ${YELLOW} "Let runing for 5 seconds ..."
 	sleep 5
-	
 
-	send_sigint ${pid6}
-	send_sigint ${pid1}
-	send_sigint ${pid5}
-	send_sigint ${pid2}
-	send_sigint ${pid3}
+	local all_pid=$(cat pid.txt | tr '\n' ' ') 
+
+	send_sigint ${display_pid}
 	sleep 1
-	send_sigint ${pid4}
+	send_sigint_all
 
-	# send_sigint ${pid6}
-	
-	# display_color_msg ${YELLOW} "Waiting for process to finish ..."
-	wait ${pid1} && wait ${pid5} && wait ${pid2} && wait ${pid3} && wait ${pid4} && wait ${pid6}
-	# display_color_msg ${YELLOW} "Waiting protect ..."
+	wait ${all_pid}
 	sleep 1
 	display_color_msg ${YELLOW} "Waiting protect finish ..."
 	./rsc/sh/check_ipcs_free.sh
 }
 
-sigint_test
+sigint_loop_test
