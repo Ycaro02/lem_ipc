b04ce40e7b4e7952db9b25d7f8aa6780dcabe616
Ycaro02 <ycaro@Kali.home>
2024-03-15 11:26:26 -0500

    add first AI logic in comment

--- src/player.c
+++ src/player.c
@@ -108,3 +108,61 @@ void player_routine(t_ipc *ipc, t_player *player)
 		usleep(100000); /* 1/10 sec */
 	}
 }
+
+
+/*
+
+enum e_player_state {
+	S_WAITING,
+	S_TRACKER,
+	S_FOLLOWER,
+};
+
+WAITING logic
+{
+	1: Player try to receive message:
+	- If message received, store value (enemy pos + first player heuristic)
+	2: Player will scan his environement X tile arround to find the best enemy to rush
+	- If no enemy found and no message receive
+		- stay in WAITING state
+		- move to a random position
+		- don't send message
+	- If enemy found and message receive :
+		- compare the heuristic of the enemy found with the heuristic of enemy position received
+	- If heuristic of the enemy is better, or no message receive:
+		- become a TRACKER
+		- move nearby to the enemy position
+		- send message to his team (enemy pos, new heuristic)
+	- Else if heuristic of the enemy is worst thans heuristic compute and heuristic receive is arround the heuristique compute
+		- become a FOLLOWER
+		- move to the position of the message received
+		- don't send message to his team (maybe just send a message to say he is following, with his heuristic cost to reach the enemy)
+	- ELse (msg receive case, if not we are in the first if case)
+		- stay in WAITING state
+		- move to position of the message received
+		- resend the receive msg to the team
+}
+
+FOLLOWER logic 
+{
+	1: Ignore message
+	2: Scan env if no enemy in X tile arround break follower logic and go WAITING case (target probably dead)
+	Else
+		- move to the position of the nearest enemy (supposed to be the position of enemy targeted by the last message received)
+		- don't send message
+		- stay a FOLLOWER
+}
+
+TRACKER logic
+{
+	1: Ignore message
+	2: Scan env if no enemy in X tile arround break tracker logic and go WAITING case (target probably dead)
+	Else
+		- compute new heuristic to reach the enemy
+		- move to the position of this enemy
+		- send message to his team (enemy pos, new heuristic) ?(maybe don't sned message)
+		- stay a TRACKER
+}
+
+
+*/
